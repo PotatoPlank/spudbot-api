3a038dc71ffb2530a8a1a9fd74f3b45845b8f571
PotatoPlank <potatoplank@protonmail.com>
2024-03-20 21:31:21 -0400

    Adding missing fields to Reminder

--- src/app/Models/Reminder.php
+++ src/app/Models/Reminder.php
@@ -18,6 +18,11 @@ class Reminder extends Model
         'repeats',
     ];
 
+    protected $with = [
+        'guild',
+        'channel',
+    ];
+
 
     public function guild(): BelongsTo
     {
